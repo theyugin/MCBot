73e4f6c6043264937a505a5233ba550dd3be84e8
theyugin <theyugin@gmail.com>
2019-07-01 08:33:22 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,163 @@
+import logging
+
+from discord import errors
+
+log_filename = 'log.txt'
+logger = logging.getLogger('MCBot')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename=log_filename, encoding='utf-8', mode='w+')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+from utils import *
+from discord.ext import commands
+import os
+import asyncio
+
+bot_token = os.environ.get('MCBOT_TOKEN')
+message_database = {}
+
+client = commands.Bot(command_prefix=commands.when_mentioned_or("mcbot "))
+
+
+async def message_updater():
+    await client.wait_until_ready()
+    while not client.is_closed():
+        db = read_database()
+        if len(db) > 0:
+            for guild, channels in db.items():
+                for channel, messages in channels.items():
+                    for message in messages:
+                        server_list = db[guild][channel][message]
+                        output = ''
+                        if len(server_list) > 0:
+                            output = None
+                            embed = status_message_generator(server_list)
+                        else:
+                            output = "No servers to monitor! Add some servers with `mcbot addserver` to be shown here."
+                            embed = None
+                        try:
+                            msg = await client.get_guild(int(guild)).get_channel(int(channel)).fetch_message(
+                                int(message))
+                            await msg.edit(content=output, embed=embed)
+                        except errors.NotFound:
+                            db[guild][channel].pop(message)
+                            remove_unused(db, guild, channel)
+                            write_database(db)
+                            logging.warning(f"MESSAGE NOT FOUND guild:{guild} channel:{channel} message:{message}")
+        await asyncio.sleep(15)
+
+
+@client.event
+async def on_message_delete(message):
+    guild, channel, id = str(message.guild.id), str(message.channel.id), str(message.id)
+    if guild in message_database:
+        if channel in message_database[guild]:
+            if message in message_database[guild][channel]:
+                message_database[guild][channel].pop(message)
+                remove_unused(message_database, guild, channel)
+                write_database(message_database)
+
+
+@client.event
+async def on_ready():
+    global message_database
+    message_database = read_database()
+    logger.info(f"Connected as {client.user}")
+
+
+@client.command("createmessage")
+async def create_message(ctx):
+    global message_database
+    await ctx.message.delete()
+    message = await ctx.send("New message created! Add some servers with `mcbot addserver` to be shown here.")
+    new_message = {str(message.guild.id): {str(message.channel.id): {str(message.id): []}}}
+    message_database = deep_dict_merge(message_database, new_message)
+    write_database(message_database)
+
+
+@client.command("removemessage")
+async def remove_message(ctx):
+    global message_database
+    cleanup = [ctx.message]
+    if not str(ctx.message.guild.id) in message_database:
+        await ctx.send("**No messages to delete in this server!**", delete_after=5)
+        await ctx.channel.delete_messages(cleanup)
+    else:
+        message_list_string, associated_messages = generate_message_link_list(message_database, ctx, client)
+        cleanup.append(await ctx.send("**Choose what message to delete:**\n" + message_list_string))
+        while True:
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            user_response = await client.wait_for('message', check=check)
+            cleanup.append(user_response)
+            message_number = user_response.content
+            if message_number.isdigit():
+                guild, channel, message = associated_messages[int(message_number) - 1]
+                if int(message_number) > len(associated_messages) or int(message_number) < 1:
+                    await user_response.delete()
+                    cleanup.remove(user_response)
+                    await ctx.send(content="Wrong message number! Try again.", delete_after=5)
+                else:
+                    message_database[guild][channel].pop(message)
+                    remove_unused(message_database, guild, channel)
+                    write_database(message_database)
+                    await ctx.channel.delete_messages(cleanup)
+                    msg = await client.get_guild(int(guild)).get_channel(int(channel)).fetch_message(int(message))
+                    await msg.delete()
+                    await ctx.send(f"Removed message #{message_number}!", delete_after=5)
+                    return
+
+
+@client.command("addserver")
+async def add_server(ctx):
+    global message_database
+    response = "**What message do you want to add your server to?**\n"
+    cleanup = [ctx.message]
+    message_list_string, associated_messages = generate_message_link_list(message_database, ctx, client)
+    response += message_list_string
+    cleanup.append(await ctx.send(response))
+
+    while True:
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
+        user_response = await client.wait_for('message', check=check)
+        cleanup.append(user_response)
+        tmp = user_response.content.split(" ")
+        if len(tmp) == 2:
+            message_number, server_address = tmp
+            if message_number.isdigit():
+                message_number = int(message_number)
+                if int(message_number) > len(associated_messages) or int(message_number) < 1:
+                    await user_response.delete()
+                    cleanup.remove(user_response)
+                    await ctx.send(content="Wrong message number! Try again.", delete_after=5)
+                else:
+                    guild, channel, message = associated_messages[message_number - 1]
+                    message_database[guild][channel][message].append(server_address)
+                    await ctx.channel.delete_messages(cleanup)
+                    await ctx.send(f"Added server {server_address} to #{message_number}!", delete_after=5)
+                    write_database(message_database)
+                    return
+
+
+@client.command("messages")
+async def messages(ctx):
+    response = "**Links to all messages in this guild:**\n"
+    message_list_string, _ = generate_message_link_list(message_database, ctx, client)
+    response += message_list_string
+    await ctx.message.delete()
+    await ctx.send(response)
+
+
+@client.command("ping")
+async def ping(ctx):
+    msg = await ctx.send('Pong!')
+    logger.info(
+        f"Ping sent in guild:{msg.guild.id}  in channel:{msg.channel.id} with message:{ctx.message.id} with response id:{msg.id}")
+
+
+client.loop.create_task(message_updater())
+client.run(bot_token)
---
+++ utils.py
@@ -0,0 +1,87 @@
+import collections
+import copy
+import json
+import os
+
+import mcstatus as mcs
+from discord import Embed
+
+database_filename = 'database.json'
+
+
+def remove_unused(db, guild, channel):
+    if len(db[guild][channel]) == 0:
+        db[guild].pop(channel)
+        if len(db[guild]) == 0:
+            db.pop(guild)
+
+
+def status_message_generator(server_list):
+    embed = Embed()
+    for server in server_list:
+        mcserver = mcs.MinecraftServer.lookup(server)
+        try:
+            status = mcserver.status()
+        except Exception as e:
+            status = "-"
+        if status == "-":
+            indicator = ":black_circle: "
+            description = '-'
+        else:
+            if status.players.online < status.players.max:
+                indicator = ":large_blue_circle: "
+            else:
+                indicator = ":red_circle: "
+            if type(status.description) is str:
+                description = status.description
+            else:
+                description = status.description["text"]
+        embed.add_field(
+            name=f"{indicator} **{server}**\n{(description if status != '-' else '-')}",
+            value=f"{status.players.online}/{status.players.max}" if status != '-' else "-"
+        )
+    return embed
+
+
+def generate_message_link_list(message_database, ctx, client):
+    response = ""
+    associated_messages = []
+    for channel, channel_dict in message_database[str(ctx.guild.id)].items():
+        response += f"Messages in {client.get_channel(int(channel)).mention}:\n"
+        for message in channel_dict:
+            monitored_message = f"https://discordapp.com/channels/{ctx.guild.id}/{channel}/{message}"
+            associated_messages.append((str(ctx.guild.id), str(channel), str(message)))
+            response += f"Message #{len(associated_messages)} {monitored_message}\n"
+    return response, associated_messages
+
+
+def read_database():
+    if not os.path.exists(database_filename):
+        f = open(database_filename, 'w+')
+        f.write('{}')
+        f.close()
+    with open(database_filename, "r") as f:
+        message_database = json.load(f)
+    return message_database
+
+
+def write_database(db):
+    with open(database_filename, 'w+') as f:
+        json.dump(db, f)
+
+
+def deep_dict_merge(dct1, dct2, override=True) -> dict:
+    merged = copy.deepcopy(dct1)
+    for k, v2 in dct2.items():
+        if k in merged:
+            v1 = merged[k]
+            if isinstance(v1, dict) and isinstance(v2, collections.Mapping):
+                merged[k] = deep_dict_merge(v1, v2, override)
+            elif isinstance(v1, list) and isinstance(v2, list):
+                merged[k] = v1 + v2
+            else:
+                if override:
+                    merged[k] = copy.deepcopy(v2)
+        else:
+            merged[k] = copy.deepcopy(v2)
+    return merged
